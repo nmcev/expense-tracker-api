258eaea0d6edecc1d4de249e39c91543eb123f3a
nmcev <1student.muhammed1@gmail.com>
2026-04-26 19:36:15 +0300

    feat: implement log requests middleware

---
+++ middleware/LogRequests.js
@@ -0,0 +1,8 @@
+
+const logRequests = ((req, res, next) => {
+    const timestamp = new Date().toISOString();
+    console.log(`${timestamp} | ${req.method} ${req.path}`);
+    next();
+})
+
+module.exports = logRequests;
