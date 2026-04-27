2e949530062a86e23de172217b13c7154f0c8172
nmcev <1student.muhammed1@gmail.com>
2026-04-28 00:07:42 +0300

    refactor: change all enum values to small letters

--- models/Expense.js
+++ models/Expense.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const categories = [
-    "Food", "Transport", "Entertainment", "Education",
-    "Clothing", "Health", "Bills", "Gifts",
-    "Housing", "Personal Care", "Groceries", "Others"
+    "food", "transport", "entertainment", "education",
+    "clothing", "health", "bills", "gifts",
+    "housing", "personal care", "groceries", "others"
 ]
 
 const expenseSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     amount: { type: Number, required: true },
-    currency: { type: String, required: true, enum: ['IQD', 'USD', 'EUR'] },
+    currency: { type: String, required: true, enum: ['idq', 'usd', 'eur'] },
     category: { type: String, enum: categories },
     description: { type: String },
     date: { type: Date },
