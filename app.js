702a4488f6e197609242aee99043e4196aad4216
nmcev <1student.muhammed1@gmail.com>
2026-04-27 16:09:16 +0300

    feat: use error middleware

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const connectToMongoDB = require("./mongoConfig");
 const logRequests = require('./middleware/LogRequests')
+
+// error middleware 
+const errorMiddleware = require('./middleware/errorMiddleware')
 // routes
 const authRoute = require('./routes/auth');
 
@@ -16,6 +19,9 @@ connectToMongoDB()
 
 
 app.use('/api', authRoute);
+
+//error middleware
+app.use(errorMiddleware)
 
 const PORT = 3000;
 app.listen(PORT, () => {
