da736b2c67c84204072a655395f65da1e3813519
nmcev <1student.muhammed1@gmail.com>
2026-04-29 20:36:13 +0300

    feat: use expense route in app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logRequests = require('./middleware/LogRequests')
 const errorMiddleware = require('./middleware/errorMiddleware')
 // routes
 const authRoute = require('./routes/auth');
+const expenseRoute = require('./routes/expense');
 
 // parse incoming json
 app.use(express.json());
@@ -19,11 +20,12 @@ connectToMongoDB()
 
 
 app.use('/api', authRoute);
+app.use('/api/expense', expenseRoute);
 
 //error middleware
 app.use(errorMiddleware)
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Server Running on http://localhost:${PORT}`)
+    console.log(`Server Running on http://localhost:${PORT}`)
 });
